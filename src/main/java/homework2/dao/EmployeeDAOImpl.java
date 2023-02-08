68afb0e00a1fb6b7870dcf62f429891dedb2de1e
Alexandra <alexandravolchihina@yandex.ru>
2023-02-08 14:09:13 +0300

    Домашнее задание к модулю 3

--- src/main/java/homework2/dao/EmployeeDAOImpl.java
+++ src/main/java/homework2/dao/EmployeeDAOImpl.java
@@ -24,75 +24,78 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public void create(Employee employee) {
-        try(PreparedStatement statement = connection.prepareStatement(
-                Queries.INSERT.query)) {
-            statement.setString(1, employee.getFirstName());
-            statement.setString(2, employee.getLastName());
-            statement.setString(3, employee.getGender());
-            statement.setInt(4, employee.getAge());
-            statement.setInt(5, employee.getCityId());
-            statement.execute();
+//        try(PreparedStatement statement = connection.prepareStatement(
+//                Queries.INSERT.query)) {
+//            statement.setString(1, employee.getFirstName());
+//            statement.setString(2, employee.getLastName());
+//            statement.setString(3, employee.getGender());
+//            statement.setInt(4, employee.getAge());
+//            statement.setInt(5, employee.getCityId());
+//            statement.execute();
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(employee);
+            transaction.commit();
         }
     }
-/*
-try(Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-    session.save(employee);
-    transaction.commit();
- */
+
     @Override
     public Employee readById(int id) {
-        Employee employee = new Employee();
+//        Employee employee = new Employee();
+//
+//        try (PreparedStatement statement = connection.prepareStatement(
+//                Queries.GET.query)) {
+//            statement.setInt(1,id);
+//            ResultSet resultSet = statement.executeQuery();
+//            while (resultSet.next()) {
+//                employee.setId(Integer.parseInt(resultSet.getString("id")));
+//                employee.setFirstName(resultSet.getString("first_name"));
+//                employee.setLastName(resultSet.getString("last_name"));
+//                employee.setGender(resultSet.getString("gender"));
+//                employee.setAge(resultSet.getInt("age"));
+//                employee.setCityId(resultSet.getInt("city_id"));
+//            }
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return employee;
+//    }
 
-        try (PreparedStatement statement = connection.prepareStatement(
-                Queries.GET.query)) {
-            statement.setInt(1,id);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                employee.setId(Integer.parseInt(resultSet.getString("id")));
-                employee.setFirstName(resultSet.getString("first_name"));
-                employee.setLastName(resultSet.getString("last_name"));
-                employee.setGender(resultSet.getString("gender"));
-                employee.setAge(resultSet.getInt("age"));
-                employee.setCityId(resultSet.getInt("city_id"));
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            return session.get(Employee.class, id);
         }
-        return employee;
     }
-/*
-try(Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
-return session.get(Employee.class, id);}
- */
     @Override
     public List<Employee> readAll() {
         List<Employee> employees = new ArrayList<>();
-        try (PreparedStatement statement = connection.prepareStatement(Queries.GET_ALL.query)){
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int id = Integer.parseInt(resultSet.getString("id"));
-                String first_name = resultSet.getString("first_name");
-                String last_name = resultSet.getString("last_name");
-                String gender = resultSet.getString("gender");
-                int age = resultSet.getInt("age");
-                int city_id = resultSet.getInt("city_id");
-                employees.add(new Employee(id, first_name, last_name, gender, age, city_id));
-            }
+//        try (PreparedStatement statement = connection.prepareStatement(Queries.GET_ALL.query)){
+//            ResultSet resultSet = statement.executeQuery();
+//            while (resultSet.next()) {
+//                int id = Integer.parseInt(resultSet.getString("id"));
+//                String first_name = resultSet.getString("first_name");
+//                String last_name = resultSet.getString("last_name");
+//                String gender = resultSet.getString("gender");
+//                int age = resultSet.getInt("age");
+//                int city_id = resultSet.getInt("city_id");
+//                employees.add(new Employee(id, first_name, last_name, gender, age, city_id));
+//            }
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return employees;
+//    }
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return employees;
-    }
-    /*
     try(Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
     return session.createQuery("From Employee").list();}
-     */
+     }
     @Override
     public void updateById(Employee employee) {
 try(Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
